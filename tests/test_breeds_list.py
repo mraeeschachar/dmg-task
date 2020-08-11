f4665586fe787a14251a41b7c6fe79b2025401ed
muhammadraees <muhammad.chachar@flaconi.de>
2020-08-11 23:16:10 +0200

    improved test

--- tests/test_breeds_list.py
+++ tests/test_breeds_list.py
@@ -1,4 +1,4 @@
-from constants import (dog_selector, boxer_breed, dog_image, fetch_dog_button
+from constants import (dog_selector, boxer_breed, dog_image, fetch_dog_button, demo_image_src
                        )
 from pages.breeds_list_page import BreedsListPage
 from tests.test_base import TestBase
@@ -14,5 +14,8 @@ class TestBreedsList(TestBase):
         """
         Verify that boxer breed can be fetched and image displays
         """
+        pre_img = self.page.find_css(demo_image_src).get_attribute("src")
         self.page.select_breed(dog_selector, boxer_breed, fetch_dog_button)
         self.assertTrue(self.page.verify_images_status_code(dog_image))
+        post_img = self.page.find_css(demo_image_src).get_attribute("src")
+        self.assertNotEqual(pre_img, post_img, "Image did not change!")
